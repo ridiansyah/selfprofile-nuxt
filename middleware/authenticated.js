143733936afd89164ef4033603cc5efc6e44cdb8
ridiansyah <wahyuridiansyah@gmail.com>
2022-10-14 10:06:16 +0700

    fix: infinity loop on auth

--- middleware/authenticated.js
+++ middleware/authenticated.js
@@ -1,21 +1,23 @@
 // Authentication middleware
 export default function Authenticated({ app, redirect }) {
   // If the user hasn't logged in, redirect to login page.
-
-  if (
-    app.$cookiz.get("selfprofile_token") === false &&
-    (app.router.history.current.fullPath !== "/login" ||
-      app.router.history.current.fullPath !== "/register" ||
-      app.router.history.current.fullPath !== "/register/otp")
-  ) {
-    console.log("masuk login");
-    return redirect("/login");
-  } else if (
-    app.$cookiz.get("selfprofile_token") &&
-    app.router.history.current.fullPath !== "/"
-  ) {
-    console.log("masuk home");
-    return redirect("/");
+  if (process.server) {
+    if (
+      app.$cookiz.get("selfprofile_token") === false &&
+      (app.router.history.current.fullPath !== "/login" ||
+        app.router.history.current.fullPath !== "/register" ||
+        app.router.history.current.fullPath !== "/register/otp")
+    ) {
+      console.log("masuk login");
+      return redirect("/login");
+    } else if (
+      app.$cookiz.get("selfprofile_token") &&
+      app.router.history.current.fullPath !== "/"
+    ) {
+      console.log("masuk home");
+      return redirect("/");
+    }
+    return true;
   }
   return true;
 }
